4ea725d34a15ba0447c6b8187c8b38665534419c
Lancer385 <ahmedkhalaf1234123@gmail.com>
2024-11-02 20:28:14 +0200

    add more linked list methods—at, pop, contains and find

--- linked-list.js
+++ linked-list.js
@@ -45,6 +45,76 @@ export class LinkedList{
         }
         return temp;
     }
+    at(index){
+        let temp = this.head;
+        let counter = 1;
+        if (index === 0){
+            return temp;
+        }
+        while (temp.next !== null){
+            temp = temp.next;
+            if (counter === index){
+                return temp;
+            };
+            counter++;
+        };
+        return null;
+    };
+    pop(){
+        if (this.length === 0){
+            return;
+        }
+        if (this.length === 1){
+            let poppedNode = this.head;
+            this.head = null;
+            this.length--;
+            console.log(this)
+            return poppedNode;
+        };
+        let temp = this.head;
+        while (temp.next.next !== null){
+            temp = temp.next;
+        };
+        let poppedNode = temp.next;
+        temp.next = null;
+        this.length--;
+        console.log(this);
+        return poppedNode;
+    };
+    contains(value){
+        if (this.head === null){
+            return false;
+        };
+        if (value === this.head.value){
+            return true;
+        };
+        let temp = this.head;
+        while (temp.next !== null){
+            temp = temp.next
+            if (value === temp.value){
+                return true
+            };
+        }
+        return false;
+    }
+    find(value){
+        if (this.head === null){
+            return null;
+        };
+        if (value === this.head.value){
+            return 0;
+        };
+        let temp = this.head;
+        let counter = 1;
+        while (temp.next !== null){
+            temp = temp.next
+            if (value === temp.value){
+                return counter;
+            };
+            counter++;
+        }
+        return null;
+    }
 }; 
 
 class Node{
